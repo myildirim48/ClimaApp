ac133d7ac615e4e92257e4c47f656ae301ad1fb1
myildirim48 <muhammedyildirim48@gmail.com>
2022-08-29 23:18:14 +0300

    WeatherManager has been developed. Added WeatherModel. JSON API data is read and ready.

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -13,5 +13,47 @@ struct WeatherManager {
     
     func fetchWeather(cityName : String){
         let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString )
     }
+    
+    func performRequest(urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
+            
+            task.resume()
+        }
+        
+    }
+    
+    func parseJSON(weatherData: Data){
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.conditionName)
+            print(weather.temperatureString)
+            
+        } catch {
+            print(error)
+        }
+    }
+    
 }
